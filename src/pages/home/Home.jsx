903ec48dbbc6d6387b53c3988735ace8f73b319d
Lakshya Gupta <lakshyag9775@gmail.com>
2022-11-20 01:11:19 +0530

    initializing home page of our website

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,32 @@
+import './home.scss';
+
+const Home = ()=>{
+    return (
+        <div className="home">
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+            <h1>home</h1>
+        </div>
+    );
+};
+
+export default Home;
