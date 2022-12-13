76ca9935f161ce36014d78b12aa407ccc8feeb45
Lakshya Gupta <lakshyag9775@gmail.com>
2022-12-14 01:05:02 +0530

    add DarkMode feature in the app

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,8 +9,17 @@ import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Link } from 'react-router-dom';
 import { GridViewOutlined } from '@mui/icons-material';
+import { useContext } from "react";
+import { DarkModeContext } from '../../context/darkModeContext';
+import { AuthContext } from '../../context/authContext';
 
 const Navbar = ()=>{
+
+    const {toggle, darkMode} = useContext(DarkModeContext);
+    const {currentUser} = useContext(AuthContext);
+
+
+
     return (
         <div className="navbar">
             <div className="left">
@@ -18,7 +27,11 @@ const Navbar = ()=>{
                     <span>Socialize</span>
                  </Link>
                 <HomeOutlinedIcon/>
-                <DarkModeOutlinedIcon/>
+                {darkMode ?
+                <WbSunnyOutlinedIcon onClick = {toggle}/>
+                :
+                <DarkModeOutlinedIcon onClick = {toggle}/>
+                }
                 <AppsOutlinedIcon/>
                 <div className="search">
                     <SearchOutlinedIcon/>
@@ -30,8 +43,8 @@ const Navbar = ()=>{
                 <EmailOutlinedIcon/>
                 <NotificationsOutlinedIcon/>
                 <div className="user">
-                    <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                    <span>Tomas</span>
+                    <img src={currentUser.profilePic} alt="" />
+                    <span>currentUser.name</span>
                 </div>
             </div>
         </div>
